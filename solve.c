d01b578d4ed53ff524ae48da41939a39def2a709
Hugo Thiessard <hthiessa@student.42.fr>
2019-02-04 16:20:12 +0100

    Remove norm issues and regroup parameters in a struct

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: hthiessa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 17:54:45 by hthiessa          #+#    #+#             */
-/*   Updated: 2019/02/04 15:47:05 by hthiessa         ###   ########.fr       */
+/*   Updated: 2019/02/04 16:19:04 by hthiessa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,9 @@ void	solve_and_print(t_tetri *tetriminos, int nb_tetri)
 	actual_width = sqrt_aprox(nb_tetri * 4);
 	while (actual_width <= MAX_WIDTH)
 	{
-		solve_and_print_rec(0, tetriminos, grid, nb_tetri, actual_width);
+		solve_and_print_rec(0, (t_solve_data){.tetriminos = tetriminos,
+					.grid = &grid, .nb_tetri = nb_tetri,
+					.actual_width = actual_width});
 		actual_width++;
 	}
 	ft_putstr_fd("Error, max width of grid reached\n", 2);
@@ -49,14 +51,14 @@ void	solve_and_print(t_tetri *tetriminos, int nb_tetri)
 }
 
 void	start_position(t_tetri *tetri, t_grid grid, long int **grid_for_cmp,
-					   long int *tetri_actual)
+						long int *tetri_actual)
 {
 	if (tetri->type->last_position)
 	{
 		tetri->position.y = tetri->type->last_position->y;
 		tetri->position.x = tetri->type->last_position->x + 1;
 		*tetri_actual = tetri->type->mask >> tetri->position.x;
-		*grid_for_cmp = (long*)(&grid[tetri->position.y]);;
+		*grid_for_cmp = (long*)(&grid[tetri->position.y]);
 	}
 	else
 	{
@@ -67,27 +69,25 @@ void	start_position(t_tetri *tetri, t_grid grid, long int **grid_for_cmp,
 	}
 }
 
-void	solve_and_print_rec(int index, t_tetri *tetriminos, t_grid grid,
-							int nb_tetri, int actual_width)
+void	solve_and_print_rec(int index, t_solve_data data)
 {
 	t_tetri		*tetri;
 	long int	*grid_for_cmp;
 	long int	tetri_actual;
 
-	if (index == nb_tetri)
-		print_and_exit(tetriminos, nb_tetri, actual_width);
-	tetri = &tetriminos[index];
-	start_position(tetri, grid, &grid_for_cmp, &tetri_actual);
-	while (tetri->position.y <= actual_width - tetri->type->height)
+	if (index == data.nb_tetri)
+		print_and_exit(data.tetriminos, data.nb_tetri, data.actual_width);
+	tetri = &data.tetriminos[index];
+	start_position(tetri, *data.grid, &grid_for_cmp, &tetri_actual);
+	while (tetri->position.y <= data.actual_width - tetri->type->height)
 	{
-		while (tetri->position.x <= actual_width - tetri->type->width)
+		while (tetri->position.x <= data.actual_width - tetri->type->width)
 		{
 			if ((*grid_for_cmp & tetri_actual) == 0)
 			{
 				*grid_for_cmp ^= tetri_actual;
 				tetri->type->last_position = &tetri->position;
-				solve_and_print_rec(index + 1, tetriminos, grid, nb_tetri,
-									actual_width);
+				solve_and_print_rec(index + 1, data);
 				*grid_for_cmp ^= tetri_actual;
 			}
 			tetri_actual >>= 1;
@@ -95,8 +95,7 @@ void	solve_and_print_rec(int index, t_tetri *tetriminos, t_grid grid,
 		}
 		tetri_actual = tetri->type->mask;
 		tetri->position.x = 0;
-		tetri->position.y++;
-		grid_for_cmp = (long*)(&grid[tetri->position.y]);
+		grid_for_cmp = (long*)(&(*data.grid)[++tetri->position.y]);
 	}
 	tetri->type->last_position = NULL;
 }
