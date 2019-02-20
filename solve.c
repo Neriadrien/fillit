80e1c57adc9fa3b3e5f01b6dafa7ab24369d8e62
Hugo Thiessard <hthiessa@student.42.fr>
2019-02-20 17:42:20 +0100

    Improve performances by skipping some pointers

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: hthiessa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 17:54:45 by hthiessa          #+#    #+#             */
-/*   Updated: 2019/02/20 16:50:54 by hthiessa         ###   ########.fr       */
+/*   Updated: 2019/02/20 17:41:20 by hthiessa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	solve_and_print(t_tetri *tetriminos, int nb_tetri)
 	data.nb_tetri = nb_tetri;
 	while (data.size <= MAX_WIDTH)
 	{
-		solve_and_print_rec(0, &data);
+		solve_and_print_rec(0, &data.ltetri[0], data.ltetri[0].type, &data);
 		data.size++;
 	}
 	ft_putstr_fd("Error, max width of grid reached\n", 2);
@@ -71,31 +71,33 @@ void	start_position(t_tetri *tetri, t_grid grid, unsigned long int **grid_for_cm
 	}
 }
 
-void	solve_and_print_rec(int index, t_solve_data *p)
+void	solve_and_print_rec(int index, t_tetri *tetri, t_type *type,
+							t_solve_data *p)
 {
 	unsigned long int	*grid_for_cmp;
 	unsigned long int	tetri_actual;
 
 	if (index == p->nb_tetri)
 		print_and_exit(p);
-	start_position(&p->ltetri[index], p->grid, &grid_for_cmp, &tetri_actual);
-	while (p->ltetri[index].pos.y <= p->size - p->ltetri[index].type->height)
+	start_position(tetri, p->grid, &grid_for_cmp, &tetri_actual);
+	while (tetri->pos.y <= p->size - type->height)
 	{
-		while (p->ltetri[index].pos.x <= p->size - p->ltetri[index].type->width)
+		while (tetri->pos.x <= p->size - type->width)
 		{
 			if ((*grid_for_cmp & tetri_actual) == 0)
 			{
 				*grid_for_cmp ^= tetri_actual;
-				p->ltetri[index].type->last_position = &p->ltetri[index].pos;
-				solve_and_print_rec(index + 1, p);
+				type->last_position = &tetri->pos;
+				solve_and_print_rec(index + 1, &p->ltetri[index + 1],
+									p->ltetri[index + 1].type, p);
 				*grid_for_cmp ^= tetri_actual;
 			}
 			tetri_actual = (unsigned long)tetri_actual >> 1;
-			p->ltetri[index].pos.x++;
+			tetri->pos.x++;
 		}
-		tetri_actual = p->ltetri[index].type->mask;
-		p->ltetri[index].pos.x = 0;
-		grid_for_cmp = (unsigned long*)(&p->grid[++p->ltetri[index].pos.y]);
+		tetri_actual = type->mask;
+		tetri->pos.x = 0;
+		grid_for_cmp = (unsigned long*)(&p->grid[++tetri->pos.y]);
 	}
-	p->ltetri[index].type->last_position = NULL;
+	type->last_position = NULL;
 }
--- solve.h
+++ solve.h
@@ -6,7 +6,7 @@
 /*   By: hthiessa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 17:54:32 by hthiessa          #+#    #+#             */
-/*   Updated: 2019/02/18 19:13:20 by hthiessa         ###   ########.fr       */
+/*   Updated: 2019/02/20 17:32:08 by hthiessa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef struct	s_solve_data
 
 void			solve_and_print(t_tetri *tetriminos, int nb_tetri);
 void			print_tab_short_int(int id, short int*tab);
-void			solve_and_print_rec(int index, t_solve_data *data);
+void			solve_and_print_rec(int index, t_tetri *tetri, t_type *type,
+									t_solve_data *data);
 
 #endif
