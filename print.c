59a90102ce9ba0fd82d56b956361ca0f7fb69da7
Hadrien BODE <hbode@student.42.fr>
2019-02-22 18:44:39 +0100

    Modif close file descriptor

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: hthiessa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 15:00:41 by hthiessa          #+#    #+#             */
-/*   Updated: 2019/02/22 17:09:30 by hthiessa         ###   ########.fr       */
+/*   Updated: 2019/02/22 18:43:49 by hbode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 
 void	error(int fd)
 {
-	ft_putendl_fd("error", 2);
-	close(fd);
+	ft_putendl_fd("error", 1);
+	if (fd >= 0)
+		close(fd);
 	exit(1);
 }
 
